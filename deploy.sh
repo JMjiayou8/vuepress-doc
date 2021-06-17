6d6e6e448ad79d595f6d14bd83063dbf6d617af9
姜敏 <jiangmin@asiainfo.com>
2021-06-17 09:44:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # 将文档上传git
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/JMjiayou8/vuepress-doc.git master
+git push -f git@github.com/JMjiayou8/vuepress-doc.git master
 
 # 生成静态文件
 npm run build
